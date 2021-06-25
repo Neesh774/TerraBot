4324e26e55ce9a6e599550a5db5b7c2e53302e6e
RiverRipple <Kanishq0106@gmail.com>
2021-06-24 20:51:44 -0400

    linter fixes

--- commands/birthdays/birthdays.js
+++ commands/birthdays/birthdays.js
@@ -1,50 +1,50 @@
-const Discord = require("discord.js");
-const config = require("../../config.json");
+const Discord = require('discord.js');
+const config = require('../../config.json');
 const bSchema = require('../../models/bday.js');
 const functions = require('../../functions.js');
 
 module.exports = {
-    name: "birthdays",
-    category: "birthdays",
-    description: "TerraBot tells you how old everyone is :)",
-    usage: `${config.prefix}birthdays`,
-    run: async (client, message, args) => {
-    //command
-    let list = await bSchema.find({});
-    let numPages = Math.ceil(list.length / 10);
-    const PS = await client.guilds.fetch(config.PS); 
-    let fields = [];
-    let start = 0;
-    let end = 10;
-    let page = 1;
-    if(list.length < 10){
-        end = list.length;
-    }
-    let numEntries = 10;
-    let arg = 1;
-    if(args[0]){
-      arg = args[0];
-    }
-    if(args[0] > numPages || args[0] < 0){
-      return message.reply("We don't seem to have that many users with birthdays yet.");
-    }
-    if(arg == numPages){
-      numEntries = list.length - 10*(numPages - 1);  
-    }
-    start = 10 * (arg - 1);
-    console.log(start)
-    end = numEntries + start;
-    console.log(end)
-    page = arg;
-    for(var i = start; i < end; i ++){
-        console.log(i);
-      fields.push({"name": `#${i+1} | ${list[i].user}`, "value": `${list[i].birthday.toString().slice(4, 10)}`})
-    }
-    let embed = new Discord.MessageEmbed()
-      .setColor(config.embedColor)
-      .setTitle(`Birthdays [${page}/${numPages}]`)
-      .addFields(fields)
-      .setAuthor("TerraBot Birthdays", PS.iconURL());
-    return message.channel.send(embed);
-    }
+	name: 'birthdays',
+	category: 'birthdays',
+	description: 'TerraBot tells you how old everyone is :)',
+	usage: `${config.prefix}birthdays`,
+	run: async (client, message, args) => {
+		// command
+		const list = await bSchema.find({});
+		const numPages = Math.ceil(list.length / 10);
+		const PS = await client.guilds.fetch(config.PS);
+		const fields = [];
+		let start = 0;
+		let end = 10;
+		let page = 1;
+		if(list.length < 10) {
+			end = list.length;
+		}
+		let numEntries = 10;
+		let arg = 1;
+		if(args[0]) {
+			arg = args[0];
+		}
+		if(args[0] > numPages || args[0] < 0) {
+			return message.reply('We don\'t seem to have that many users with birthdays yet.');
+		}
+		if(arg == numPages) {
+			numEntries = list.length - 10 * (numPages - 1);
+		}
+		start = 10 * (arg - 1);
+		console.log(start);
+		end = numEntries + start;
+		console.log(end);
+		page = arg;
+		for(let i = start; i < end; i++) {
+			console.log(i);
+			fields.push({ 'name': `#${i + 1} | ${list[i].user}`, 'value': `${list[i].birthday.toString().slice(4, 10)}` });
+		}
+		const embed = new Discord.MessageEmbed()
+			.setColor(config.embedColor)
+			.setTitle(`Birthdays [${page}/${numPages}]`)
+			.addFields(fields)
+			.setAuthor('TerraBot Birthdays', PS.iconURL());
+		return message.channel.send(embed);
+	},
 };
--- commands/suggestions/suggestions.js
+++ commands/suggestions/suggestions.js
@@ -1,24 +1,24 @@
-const Discord = require("discord.js")
-const config = require("../../config.json");
-const sSchema = require("../../models/suggestschema");
+const Discord = require('discord.js');
+const config = require('../../config.json');
+const sSchema = require('../../models/suggestschema');
 
 module.exports = {
-    name: "suggestions",
-    category: "suggestions",
-    description: "Lists all suggestions",
-    usage: `${config.prefix}suggestions`,
-    run: async (client, message, args) => {
-    //command
-    let fields = [];
-    const numSuggest = await sSchema.countDocuments({});
-    for(var i = 1;i < numSuggest + 1;i ++){
-        const suggest = await sSchema.findOne({id: i}).exec();
-        fields.push({"name": `#${i} ${suggest.createdBy} | ${suggest.status}`, "value": `${suggest.suggestion} | 👍 ${suggest.upvotes} 👎 ${suggest.downvotes}`});
-    }
-    let embed = new Discord.MessageEmbed()
-        .setColor(config.embedColor)
-        .setTitle("Suggestions for " + message.guild.name)
-        .addFields(fields);
-    return message.channel.send(embed);
-    }
+	name: 'suggestions',
+	category: 'suggestions',
+	description: 'Lists all suggestions',
+	usage: `${config.prefix}suggestions`,
+	run: async (client, message, args) => {
+		// command
+		const fields = [];
+		const numSuggest = await sSchema.countDocuments({});
+		for(let i = 1;i < numSuggest + 1;i++) {
+			const suggest = await sSchema.findOne({ id: i }).exec();
+			fields.push({ 'name': `#${i} ${suggest.createdBy} | ${suggest.status}`, 'value': `${suggest.suggestion} | 👍 ${suggest.upvotes} 👎 ${suggest.downvotes}` });
+		}
+		const embed = new Discord.MessageEmbed()
+			.setColor(config.embedColor)
+			.setTitle('Suggestions for ' + message.guild.name)
+			.addFields(fields);
+		return message.channel.send(embed);
+	},
 };
